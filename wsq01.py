6b3a76d3685e635fa15a5ed11bd89b7b2a7af812
Arual49 <42259308+Arual49@users.noreply.github.com>
2018-08-15 23:35:08 -0500

    Add files via upload

---
+++ wsq01.py
@@ -0,0 +1,15 @@
+print ("Welcome! Please give me 2 numbers!")
+x = int(input ("Number 1: "))
+y = int(input ("Number 2: "))
+
+adding = str(x+y)
+resting = str(x-y)
+multiplying = str(x*y)
+divide = str(x//y)
+rest = str(x%y)
+
+print ("The sum would be: " + adding)
+print ("The rest would be: " + resting)
+print ("The product would be: " + multiplying)
+print ("The division would be: " + divide)
+print ("The remainder would be: " + rest)
